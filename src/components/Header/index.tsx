04aec480bfbd97eb606c8ab865d149ba061c450c
joseeduardotomazeli <joseeduardo.tomazeli@outlook.com>
2021-03-26 02:01:20 -0300

    feat: add signin button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import SignInButton from '../SignInButton';
+
 import styles from './styles.module.scss';
 
 function Header() {
@@ -10,6 +12,8 @@ function Header() {
           <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   );
