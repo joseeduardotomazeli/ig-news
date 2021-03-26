8581c7a214381f79892e7996dafb68798d4d6991
joseeduardotomazeli <joseeduardo.tomazeli@outlook.com>
2021-03-26 02:15:54 -0300

    feat: add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,30 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss';
+
 function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
 
-      <h1>ig.news</h1>
+      <main className={styles.homeContainer}>
+        <section className={styles.homeHero}>
+          <span>👏 Hey, welcome</span>
+
+          <h1>
+            News about <br />
+            the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
