4ae09777d1461d71c061b357e58d777286f535c3
joseeduardotomazeli <joseeduardo.tomazeli@outlook.com>
2021-03-26 02:26:36 -0300

    feat: add subscribe button component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 
+import SubscribeButton from '../components/SubscribeButton';
+
 import styles from './home.module.scss';
 
 function Home() {
@@ -21,6 +23,8 @@ function Home() {
             Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
